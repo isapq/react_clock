3494057e5a0c0b81fcf77bc8a0e80af3b6b075a0
Isaque Pereira dos Santos <isaquevidaltube@gmail.com>
2025-08-22 17:46:58 -0300

    solution after code review

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+//import React, { useEffect, useState } from 'react';
 import './App.scss';
+import { Clock } from './components/clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -18,24 +19,28 @@ export class App extends React.Component<{}, State> {
 
   private timeTimerId?: number;
 
-  constructor(props: {}) {
-    super(props);
+  state: State = {
+    hasClock: true,
+    clockName: 'Clock-0',
+    time: new Date().toLocaleTimeString(),
+  };
 
-    this.state = {
-      hasClock: true,
-      clockName: 'Clock-0',
-      time: new Date().toLocaleTimeString(),
-    };
-  }
-
+  /* eslint-disable */
   componentDidMount(): void {
     this.nameTimerId = window.setInterval(() => {
       this.setState({ clockName: getRandomName() });
     }, 3300);
 
     this.timeTimerId = window.setInterval(() => {
-      this.setState({ time: new Date().toLocaleTimeString() });
+      const currentTime = new Date().toUTCString().slice(-12, -4);
+
+      this.setState({ time: currentTime });
+
+      console.log(currentTime);
     }, 1000);
+
+    document.addEventListener('contextmenu', this.handleContextMenu);
+    document.addEventListener('click', this.handleClick);
   }
 
   componentWillUnmount(): void {
@@ -46,7 +51,20 @@ export class App extends React.Component<{}, State> {
     if (this.timeTimerId) {
       clearInterval(this.timeTimerId);
     }
+
+    document.removeEventListener('contextmenu', this.handleContextMenu);
+    document.removeEventListener('click', this.handleClick);
   }
+
+  handleContextMenu = (e: MouseEvent) => {
+    e.preventDefault();
+    console.log('Clique com o botão direito bloqueado!');
+  };
+
+  handleClick = (e: MouseEvent) => {
+    console.log('Clique detectado no documento');
+  };
+  /* eslint-disable */
 
   render(): React.ReactNode {
     const { hasClock, clockName, time } = this.state;
@@ -55,13 +73,7 @@ export class App extends React.Component<{}, State> {
       <div className="App">
         <h1>React clock</h1>
 
-        {hasClock && (
-          <div className="Clock">
-            <strong className="Clock__name">{clockName}</strong>
-            {' time is '}
-            <span className="Clock__time">{time}</span>
-          </div>
-        )}
+        {hasClock && <Clock clock={clockName} time={time} />}
       </div>
     );
   }
